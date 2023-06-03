7101e3a3f7bdb33c46a975fab4524d40d62f5cc5
unknown <sachin3145@outlook.com>
2023-06-03 16:21:49 +0530

    socket connections added

---
+++ server.js
@@ -0,0 +1,54 @@
+const Express = require('express');
+const http = require('http');
+const ACTIONS = require('./src/Actions');
+const { Server } = require('socket.io');
+const { rmSync } = require('fs');
+
+const app = Express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const userSocketMap = {
+
+}
+
+function getAllConnectedClients(roomId) {
+    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(socketId => {
+        return {
+            socketId,
+            userName: userSocketMap[socketId]
+        }
+    });
+}
+
+io.on('connection', socket => {
+    console.log("socket connected " + socket.id);
+    socket.on(ACTIONS.JOIN, ({roomId, userName}) => {
+        userSocketMap[socket.id] = userName;
+        socket.join(roomId);
+        const clients = getAllConnectedClients(roomId);
+        clients.forEach(({ socketId }) => {
+            io.to(socketId).emit(ACTIONS.JOINED, {
+                clients,
+                userName,
+                socketId: socket.id,
+            });
+        })
+    });
+    socket.on('disconnecting', () => {
+        const rooms = [...socket.rooms];
+        rooms.forEach(roomId => {
+            socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
+                socketId: socket.id,
+                userName: userSocketMap[socket.id]
+            });
+        })
+        delete userSocketMap[socket.id];
+        socket.leave();
+    });
+})
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log("Listening on port " + PORT);
+})
--- src/Pages/EditorPage.jsx
+++ src/Pages/EditorPage.jsx
@@ -1,14 +1,63 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Client from "../components/Clients";
 import Editor from "../components/Editor";
+import { initSocket } from "../socket";
+import { useNavigate, Navigate, useLocation, useParams } from "react-router-dom";
+import { toast } from "react-hot-toast";
+
+const ACTIONS = require('../Actions');
 
 function EditorPage() {
-  const [clients, setClients] = useState([
-    { socketId: 1, userName: "Sachin" },
-    { socketId: 2, userName: "Devansh" },
-    { socketId: 3, userName: "Rahul" },
+  const socketRef = useRef(null);
+  const location = useLocation();
+  const reactNavigator = useNavigate();
+  const { roomId } = useParams();
 
-  ]);
+  const [clients, setClients] = useState([]);
+  useEffect(() => {
+    async function init() {
+      socketRef.current = await initSocket();
+      socketRef.current.on('connect_error', err => handleErrors(err));
+      socketRef.current.on('connect_failed', (err) => handleErrors(err));
+
+      function handleErrors(e) {
+        console.log('socket error', e);
+        toast.error(`socket connection failed, try again later :/`);
+        reactNavigator('/');
+      }
+      socketRef.current.emit(ACTIONS.JOIN, {
+        roomId,
+        userName: location.state?.userName,
+      });
+
+      socketRef.current.on(ACTIONS.JOINED, ({ clients, userName, socketId }) => {
+        if (userName !== location.state?.userName) {
+          toast.success(`${userName} joined the room`);
+        }
+        setClients(clients);
+      });
+
+      socketRef.current.on(ACTIONS.DISCONNECTED, ({ socketId, userName }) => {
+        toast.success(`${userName} left the room`);
+        setClients((prev) => {
+          return prev.filter(client => client.socketId !== socketId);
+        });
+      });
+
+    }
+    init();
+    return () => {
+      socketRef.current.disconnect();
+      socketRef.current.off(ACTIONS.JOINED);
+      socketRef.current.off(ACTIONS.DISCONNECTED);
+    }
+  }, []);
+
+
+
+  if (!location.state) {
+    <Navigate to="/" />;
+  }
 
   return (
     <div className="mainWrap">
